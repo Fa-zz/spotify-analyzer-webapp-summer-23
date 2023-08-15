2b2dfea16bb8554b51376b4aafa1d9514e4b2a56
Fa-zz <fansar8712@gmail.com>
2023-08-15 19:12:40 -0400

    Frontend of displaying artists works, backend of displaying tracks does not. Will change layout of frontend

--- app/userdata/views.py
+++ app/userdata/views.py
@@ -1,6 +1,6 @@
 import requests
 from flask import redirect, request, session, url_for, render_template, current_app
-from .forms import DropdownForm, DD_TYPE_CHOICES, DD_NUMBER_CHOICES, DD_TIME_FRAME_CHOICES
+from .forms import DropdownForm, DD_TYPE_CHOICES, DD_TIME_FRAME_CHOICES
 from . import userdata
 import spotipy
 
@@ -23,39 +23,41 @@ def top_tracks_data_clean(data):
 
 def top_artist_data_clean(data):
     names = []
-    image_info = {}
-    for item in data['items']:
+    img_urls = {}
+    pops = {}
+    for i, item in enumerate(data['items']):
         names.append(item['name'])
-        image_info[item['name']] = (item['images'][2])
-    return names, image_info
+        img_urls[item['name']] = item['images'][2]['url']
+        pops[item['name']] = item['popularity']
+    return names, img_urls, pops
 
 
-def get_data(type, number_disp, time_frame):
-    access_token = session.get('access_token')
-    if access_token:
-        headers = {'Authorization': f'Bearer {access_token}'}
-    else:
-        return False
-
-    if time_frame == 'Four weeks':
-        time = 'short_term'
-    elif time_frame == 'Six months':
-        time = 'medium_term'
-    elif time_frame == 'All time':
-        time = 'long_term'
-    else:
-        return False
-
-    type = type.lower()
-
-    data_request_url = f'https://api.spotify.com/v1/me/top/{type}?time_range={time}&limit={number_disp}&offset=0'
-
-    info = requests.get(
-        data_request_url, headers=headers)
-    # user_info = requests.get(
-    #     'https://api.spotify.com/v1/me', headers=headers).json()
-
-    print(type, info)
+# def get_data(type, number_disp, time_frame):
+#     access_token = session.get('access_token')
+#     if access_token:
+#         headers = {'Authorization': f'Bearer {access_token}'}
+#     else:
+#         return False
+#
+#     if time_frame == 'Four weeks':
+#         time = 'short_term'
+#     elif time_frame == 'Six months':
+#         time = 'medium_term'
+#     elif time_frame == 'All time':
+#         time = 'long_term'
+#     else:
+#         return False
+#
+#     type = type.lower()
+#
+#     data_request_url = f'https://api.spotify.com/v1/me/top/{type}?time_range={time}&limit={number_disp}&offset=0'
+#
+#     info = requests.get(
+#         data_request_url, headers=headers)
+#     # user_info = requests.get(
+#     #     'https://api.spotify.com/v1/me', headers=headers).json()
+#
+#     print(type, info)
 
     # session['user_name'] = user_info['display_name']
     # session['followers'] = user_info['followers']['total']
@@ -75,7 +77,7 @@ def get_data(type, number_disp, time_frame):
     #     session['image_info'] \
     #         = top_tracks_data_clean(info)
 
-    return True
+    # return True
 
 
 @userdata.route('/profile', methods=['GET', 'POST'])
@@ -86,27 +88,43 @@ def profile():
         return redirect('/')
     spotify = spotipy.Spotify(auth_manager=auth_manager)
 
-    return spotify.current_user_top_artists()
-
     my_form = DropdownForm()
-    if my_form.validate_on_submit():
-        type = my_form.get_choice_label(DD_TYPE_CHOICES, my_form.dd_type.data)
-        number_disp = int(my_form.get_choice_label(DD_NUMBER_CHOICES, my_form.dd_number.data))
-        time_frame = my_form.get_choice_label(DD_TIME_FRAME_CHOICES, my_form.dd_time_frame.data)
-        print(f'Number to display: {number_disp}')
-        print(f'Time frame: {time_frame}')
+    if my_form.is_submitted():
+        type = my_form.dd_type.data
+        range = my_form.dd_time_frame.data
+        print(type, range)
     else:
         print("Form did not submit")
         type = "Artists"
-        number_disp = int(5)
-        time_frame = "Four weeks"
+        range = "short_term"
 
-    got_data = get_data(type, number_disp, time_frame)
+    if range == 'short_term':
+        time_frame = 'Four Weeks'
+    elif range == 'medium_term':
+        time_frame = 'Six Months'
+    elif range == 'long_term':
+        time_frame = 'All time'
+    else:
+        time_frame = 'short_term'
 
     if time_frame != "All time":
         time_frame = " the Past " + time_frame
 
-    string = f"Your Top {number_disp} Most Streamed {type} of {time_frame}"
+    # got_data = get_data(type, number_disp, time_frame)
+    artist_results = spotify.current_user_top_artists(time_range=range, limit=50)
+    artist_names, artist_imgs, artist_pops = top_artist_data_clean(artist_results)
+
+    string = f"Your Most Streamed {type.capitalize()} of {time_frame}"
+    return render_template('userdata/profile.html',
+                           form=my_form,
+                           string=string,
+                           type=type,
+                           user=spotify.me()['display_name'],
+                           followers=spotify.me()['followers']['total'],
+                           artists=artist_names,
+                           artist_imgs=artist_imgs,
+                           artist_pops=artist_pops
+                           )
 
     # if got_data:
     #     return "hello"
