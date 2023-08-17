41215d11d593aeb01042d03f6d9b41537f8180ee
Fa-zz <fansar8712@gmail.com>
2023-08-17 14:52:46 -0400

    Front/back end for sorting tracks by their popularity

--- app/userdata/views.py
+++ app/userdata/views.py
@@ -5,26 +5,26 @@ from . import userdata
 import spotipy
 
 
-def top_tracks_data_clean(data):
-    track_names = []
-    artist_names = {}
-    img_urls = {}
-    pops = {}
+def top_tracks_data_clean(data, sort_by):
+    track_dict = {}
+    sorted_track_dict = {}
     for i, item in enumerate(data['items']):
-        name = item['name']
-        track_names.append(name)
-        artist_names[name] = item['artists'][0]['name']
-        img_urls[name] = item['album']['images'][2]['url']
-        pops[name] = item['popularity']
-    return track_names, artist_names, img_urls, pops
+        track_name = item['name']
+        track_dict[track_name] = [item['artists'][0]['name']]
+        track_dict[track_name].append(item['popularity'])
+        if sort_by == "popularity":
+            sorted_track_dict = dict(sorted(track_dict.items(), key=lambda item: item[1][1], reverse=True))
+        else:
+            sorted_track_dict = track_dict
+
+        sorted_track_dict[track_name].append(item['album']['images'][2]['url'])
+
+    return sorted_track_dict
 
 
 def top_artist_data_clean(data, sort_by):
-    # Find urls for artists only after sorting
-    names = []
-    img_urls = []
-    pops = {}
     artist_dict = {}
+    sorted_artist_dict = {}
     for i, item in enumerate(data['items']):
         name = item['name']
         artist_dict[name] = [item['popularity']]
@@ -34,57 +34,10 @@ def top_artist_data_clean(data, sort_by):
             sorted_artist_dict = artist_dict
 
         sorted_artist_dict[name].append(item['images'][2]['url'])
-        # result_dict = {key: [value] for key, value in list_of_tuples}
 
     return sorted_artist_dict
 
 
-# def get_data(type, number_disp, time_frame):
-#     access_token = session.get('access_token')
-#     if access_token:
-#         headers = {'Authorization': f'Bearer {access_token}'}
-#     else:
-#         return False
-#
-#     if time_frame == 'Four weeks':
-#         time = 'short_term'
-#     elif time_frame == 'Six months':
-#         time = 'medium_term'
-#     elif time_frame == 'All time':
-#         time = 'long_term'
-#     else:
-#         return False
-#
-#     type = type.lower()
-#
-#     data_request_url = f'https://api.spotify.com/v1/me/top/{type}?time_range={time}&limit={number_disp}&offset=0'
-#
-#     info = requests.get(
-#         data_request_url, headers=headers)
-#     # user_info = requests.get(
-#     #     'https://api.spotify.com/v1/me', headers=headers).json()
-#
-#     print(type, info)
-
-    # session['user_name'] = user_info['display_name']
-    # session['followers'] = user_info['followers']['total']
-    # if type == 'artists':
-    #     session['artists'], \
-    #     session['artist_img_infos'] \
-    #         = top_artist_data_clean(info)
-    #     session['track_names'] = ''
-    #     session['track_popularities'] = ''
-    #     session['artist_names'] = ''
-    #     session['image_info'] = ''
-    #
-    # elif type == 'tracks':
-    #     session['track_names'], \
-    #     session['track_popularities'], \
-    #     session['artist_names'], \
-    #     session['image_info'] \
-    #         = top_tracks_data_clean(info)
-
-    # return True
 
 #TODO: Hover link to spotify
 #TODO: For artists you can sort by popularity, # albums, and possibly audio features?. For tracks you can sort by popularity, release date, audio features
@@ -125,11 +78,11 @@ def profile():
     artist_results = spotify.current_user_top_artists(time_range=range_to_get, limit=50)
     sorted_artist_dict = top_artist_data_clean(artist_results, sort_by)
     track_results = spotify.current_user_top_tracks(time_range=range_to_get, limit=50)
-    track_names, track_artist_names, track_imgs, track_pops = top_tracks_data_clean(track_results)
+    sorted_track_dict = top_tracks_data_clean(track_results, sort_by)
     string = f"Your Most Streamed {type.capitalize()} of {time_frame}."
 
     if sort_by == "unsorted":
-        string = string + f" Sorted by Listens"
+        string = string + f" Sorted by Your Listens"
     else:
         string = string + f" Sorted by {sort_by.capitalize()}"
 
@@ -141,10 +94,6 @@ def profile():
                            followers=spotify.me()['followers']['total'],
 
                            artist_dict=sorted_artist_dict,
-
-                           track_names=track_names,
-                           track_artist_names=track_artist_names,
-                           track_imgs=track_imgs,
-                           track_pops=track_pops,
+                           track_dict=sorted_track_dict
                         )
 
